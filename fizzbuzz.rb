0fad4a383d425ea5dacb4d2de5dcc285c247fa73
Vicki Hamm <vicki.cello@gmail.com>
2018-06-13 13:37:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-elsif int % 5 == 0
-  "Buzz"
-elsif int % 15 == 0
-  "FizzBuzz"
-else
-  nil
+   elsif int % 5 == 0
+    "Buzz"
+  elsif int % 15 == 0
+    "FizzBuzz"
+  else
+    nil
+  end
 end
